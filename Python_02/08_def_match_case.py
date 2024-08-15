bb70cbd8fe82734d54c2816a3af1eae19f558a38
mateus coelho <130664592+mateuscoelho07@users.noreply.github.com>
2024-08-15 07:46:16 -0300

    s
    exemplos

---
+++ Python_02/08_def_match_case.py
@@ -0,0 +1,18 @@
+sabor = int(input("Indique o sabor desejado: "))
+
+# Função para selecionar sabor
+def sabor_pizza(sabor):
+    match sabor:
+         case 1:
+            return print("Mussarela")
+         case 2:
+            return print("Calabresa")
+         case 3:
+            return print("Frango c/catupiry")
+         case 4:
+            return print("Portuguesa")
+         case _:
+            return print("Sabor inválido!")
+       
+# exibição 
+sabor_pizza(sabor)
